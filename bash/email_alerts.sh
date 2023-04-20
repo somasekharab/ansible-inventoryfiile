680662183f406089123e06167379bc22eefc999b
somasekharab <123715624+somasekharab@users.noreply.github.com>
2023-04-20 16:01:46 +0530

    Create email_alerts.sh

---
+++ bash/email_alerts.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+To="gowdassekhar2003@gmail.com"
+ram_free=$(free -mt | grep total: | awk '{print $4}')
+
+if [ $ram_free -le 700]
+then
+   echo "sending email become your ram free size is lessthan 700"
+   echo "subject:warning.ram free size is low " | send mail $To
+fi
